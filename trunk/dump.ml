a635d1a88aa14a0fc3c92520620a8ea73d8c737a
jonathan@kimmitt.co.uk <jonathan@kimmitt.co.uk@94572981-9c94-205d-b4b6-20de8af057cf>
2010-08-24 10:29:23 +0000

    Correct block comments

--- trunk/dump.ml
+++ trunk/dump.ml
@@ -1,21 +1,21 @@
-//*****************************************************************************
-//
-//    DESCRIPTION: Verilog parser dump parser tree file
-//
-//*****************************************************************************
-//
-// Copyright 2010 by Jonathan Kimmitt.  This program is free software; you can
-// redistribute it and/or modify it under the terms of either the GNU
-// General Public License or the Perl Artistic License.
-//
-// This code is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-//*****************************************************************************
-// Based on verilator parser code by Paul Wasson, Duane Galbi and Wilson Snyder
-//*****************************************************************************
+(******************************************************************************
+*
+*    DESCRIPTION: Verilog parser dump parser tree file
+*
+******************************************************************************
+*
+* Copyright 2010 by Jonathan Kimmitt.  This program is free software; you can
+* redistribute it and/or modify it under the terms of either the GNU
+* General Public License or the Perl Artistic License.
+*
+* This code is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+******************************************************************************
+* Based on verilator parser code by Paul Wasson, Duane Galbi and Wilson Snyder
+*******************************************************************************)
 
 open Printf
 open Vparser
