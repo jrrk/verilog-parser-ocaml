68dc56185cfd4800fe8c466f394d68e46cab87b5
jonathan@kimmitt.co.uk <jonathan@kimmitt.co.uk@94572981-9c94-205d-b4b6-20de8af057cf>
2010-08-25 17:47:05 +0000

    Separate parser from main

---
+++ trunk/parse.ml
@@ -0,0 +1,30 @@
+(******************************************************************************
+*
+*    DESCRIPTION: Verilog parser program
+*
+******************************************************************************
+*
+* Copyright 2010 by Jonathan Kimmitt.  This program is free software; you can
+* redistribute it and/or modify it under the terms of either the GNU
+* General Public License or the Perl Artistic License.
+*
+* This code is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+******************************************************************************
+* Based on verilator parser code by Paul Wasson, Duane Galbi and Wilson Snyder
+*******************************************************************************)
+
+let parse () =
+  let ic = open_in Sys.argv.( Array.length Sys.argv - 1 ) in
+  try
+    let lexbuf = Lexing.from_channel ic in
+    while true do
+      Grammar.start Lexer.token lexbuf
+    done
+    
+  with End_of_file ->
+    close_in_noerr ic;
+    Hashtbl.iter Dump.moditer Globals.modules
