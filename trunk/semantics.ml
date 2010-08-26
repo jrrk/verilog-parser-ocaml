e90d0e46b3d804e2f357cb2aa62506ce26654a71
jonathan@kimmitt.co.uk <jonathan@kimmitt.co.uk@94572981-9c94-205d-b4b6-20de8af057cf>
2010-08-26 15:28:52 +0000

    Change error handling

--- trunk/semantics.ml
+++ trunk/semantics.ml
@@ -27,11 +27,11 @@ let range hi lo = printf "[%d:%d] " (Nativeint.to_int hi) (Nativeint.to_int lo);
 
 let enter_sym symbols id attr = 
 if Hashtbl.mem symbols id then begin
-  printf "Update %s: %s\n" id (Ord.getstr attr);
+(*  printf "Update %s: %s\n" id (Ord.getstr attr); *)
   let newset = (Hashtbl.find symbols id) in
   Hashtbl.replace symbols id (TokSet.add attr newset); end
 else begin
-  printf "Enter %s: %s\n" id (Ord.getstr attr);
+(*  printf "Enter %s: %s\n" id (Ord.getstr attr); *)
   Hashtbl.add symbols id (TokSet.singleton (ID id))
   end
 ;;
@@ -40,35 +40,31 @@ let iter_ semantics syms list =
   List.iter (fun x -> semantics ({Globals.tree=x; symbols=syms})) list
 ;;
 
+let unhandled arg = ignore(Ord.getstr(arg));;
+
 let rec semantics (tree:Globals.modtree) =
    let exp = tree.Globals.tree and syms = tree.Globals.symbols in match exp with
-| DOUBLE(tok,arg) -> printf "%s " (Ord.getstr(tok)) ; iter_ semantics syms arg
 | TRIPLE(EQUALS,arg1,arg2) -> iter_ semantics syms arg1; iter_ semantics syms arg2
 | TRIPLE(IF,arg1,arg2) -> iter_ semantics syms arg1; iter_ semantics syms arg2
 | TRIPLE(PLUS,arg1,arg2) -> iter_ semantics syms arg1; iter_ semantics syms arg2
-| TRIPLE(tok,arg1,arg2) -> printf "%s " (Ord.getstr(tok)) ; iter_ semantics syms arg1; iter_ semantics syms arg2
 | QUADRUPLE(EQUALS,arg1,arg2,arg3) -> iter_ semantics syms arg1; iter_ semantics syms arg2; iter_ semantics syms arg3
 | QUADRUPLE(IF,arg1,arg2,arg3) -> iter_ semantics syms arg1; iter_ semantics syms arg2; iter_ semantics syms arg3
-| QUADRUPLE(tok,arg1,arg2,arg3) -> printf "%s " (Ord.getstr(tok)) ; iter_ semantics syms arg1; iter_ semantics syms arg2; iter_ semantics syms arg3
 | QUINTUPLE(MODULE,[ID arg1],arg2,arg3,arg4) ->
     enter_sym syms arg1 MODULE;
     iter_ semantics syms arg2;
-    iter (fun arg -> match arg with ID id -> enter_sym syms id IOPORT | _ -> printf "Error ") arg3;
+    iter (fun arg -> match arg with ID id -> enter_sym syms id IOPORT | _ -> unhandled arg) arg3;
     iter_ semantics syms arg4
-| QUINTUPLE((INPUT|OUTPUT|INOUT) as dir,arg1,arg2,arg3,arg4) ->
+| QUINTUPLE((INPUT|OUTPUT|INOUT|REG) as dir,arg1,arg2,arg3,arg4) ->
     iter_ semantics syms arg1;
     iter_ semantics syms arg2;
-    (match arg3 with [RANGE([INTNUM hi],[INTNUM lo])] -> range hi lo
-      | _ ->  List.iter (fun x -> printf "unmatched arg3 %s" (Ord.getstr x)) arg3);
+    (match arg3 with [RANGE([INTNUM hi],[INTNUM lo]) as rangehilo] -> range hi lo
+      | _ ->  List.iter unhandled arg3);
     ( match arg4 with [DOUBLE(ID id,arg5)] ->
       enter_sym syms id dir
       | [TRIPLE(ID id,arg5,arg6)] -> enter_sym syms id dir
-      | _ ->  List.iter (fun x -> match x with TRIPLE(ID id,arg5,arg6) -> enter_sym syms id dir | _ -> printf "unmatched arg4 %s\n" (Ord.getstr x)) arg4);
-| QUINTUPLE(tok,arg1,arg2,arg3,arg4) -> printf "%s " (Ord.getstr(tok)) ; iter_ semantics syms arg1; iter_ semantics syms arg2; iter_ semantics syms arg3; iter_ semantics syms arg4
-| SEXTUPLE(tok,arg1,arg2,arg3,arg4,arg5) -> printf "%s " (Ord.getstr(tok)) ; iter_ semantics syms arg1; iter_ semantics syms arg2; iter_ semantics syms arg3; iter_ semantics syms arg4; iter_ semantics syms(arg5)
-| SEPTUPLE(tok,arg1,arg2,arg3,arg4,arg5,arg6) -> printf "%s " (Ord.getstr(tok)) ; iter_ semantics syms arg1; iter_ semantics syms arg2; iter_ semantics syms arg3; iter_ semantics syms arg4; iter_ semantics syms(arg5); iter_ semantics syms(arg6)
+      | _ ->  List.iter (fun x -> match x with TRIPLE(ID id,arg5,arg6) -> enter_sym syms id dir | _ -> unhandled x) arg4);
 | RANGE(arg1,arg2) -> iter_ semantics syms arg1; iter_ semantics syms arg2
 | ID id -> enter_sym syms id EMPTY
-| _ -> printf "Unhandled %s\n" (Ord.getstr exp);;
+| _ -> unhandled(exp);;
 
 let moditer k (x:Globals.modtree) = semantics x;;
